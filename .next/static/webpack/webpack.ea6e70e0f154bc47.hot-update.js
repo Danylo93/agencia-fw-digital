6313bf8c8a9665d9d65a6a3c1156fd8086b2641e
Danylo Oliveira <danylo.oliveira73@gmail.com>
2023-11-09 11:26:26 -0300

    favicon

--- .next/static/webpack/webpack.ea6e70e0f154bc47.hot-update.js
+++ .next/static/webpack/webpack.ea6e70e0f154bc47.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "bce46af66926139b"; }
+/******/ 	__webpack_require__.h = function() { return "68aacbdda7e22e18"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -17,7 +17,7 @@ class MyDocument extends Document {
           <meta name="description" content="ruko - Onepage Multi-Purpose Reactjs Template" />
           <meta name="author" content="" />
 
-          <link rel="shortcut icon" href="/assets/img/favicon.ico" />
+          <link rel="shortcut icon" href="/assets/img/logo.png" />
           <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css?family=Teko:300,400,500,600,700&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,500,600,700,800,900&display=swap" rel="stylesheet" />
