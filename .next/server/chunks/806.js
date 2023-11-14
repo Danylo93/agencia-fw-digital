cd78161a03012d5bae66776bbc87cb8337b9c47d
Danylo Oliveira <danylo.oliveira73@gmail.com>
2023-11-14 11:10:04 -0300

    teste

---
+++ .next/server/chunks/806.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.id = 806;
+exports.ids = [806];
+exports.modules = {
+
+/***/ 2806:
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"E8":"/assets/img/logo.png","QP":true,"Lk":{"gT":"Rua Crispim Gonçalves","n5":"São Paulo","SB":"SP","V2":"Brasil"},"Do":"contato@agenciafwdigital.com","m7":"(11)98362-6321","Wd":"Copyright 2023 © All Right Reserved Design by","Nl":"Agência FW Digital"}');
+
+/***/ })
+
+};
+;
